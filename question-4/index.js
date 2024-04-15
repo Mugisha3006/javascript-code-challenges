da40734da71aee3f75ed40d7947a4476dae03395
mugisha <mohamugi@gmail.com>
2024-04-15 17:05:25 +0300

    solution to question 5

--- question-4/index.js
+++ question-4/index.js
@@ -3,12 +3,16 @@ A string is palindromic if it reads the same forward and backward.
  */
 
 let words = ["abc", "car", "ada", "racecar", "cool"];
-
+let size = words.length;
+// create  isPalindrome function
+// function isPalindrome(words) {
+//     // replicate string words into string items
+//     let items = words;
+//     words = words, split("").reverse().join("");
+//     // check if two strings are equal or not
+//     return words == items;
+// }
+// let result = isPalindrome(words)
+// console.log(result) 
 // creating a function firstPalindrome
-function firstPalindrome(words) {
-    for (let i = 0; i < words.length; i++) {
-        if () {
-            
-        }
-    }   
-}
+ 
--- question-5/index.js
+++ question-5/index.js
@@ -5,3 +5,23 @@
 let num = 38;
 
 // create a function
+function getAddedDigits(num) {
+  // convert the number to a string
+  let numToString = num.toString();
+  // initiate sum variable to zero
+  let sum = 0;
+  // iterate through the num string 
+  for (let number of numToString) {
+    // getting sum of the digits
+    sum = sum + parseInt(number);
+  }
+  // check whether the sum is equal to two digits
+  if (sum >= 10) {
+    return getAddedDigits(sum);
+  } else {
+    return sum;
+  }
+}
+
+let result = getAddedDigits(num);
+console.log(result);
