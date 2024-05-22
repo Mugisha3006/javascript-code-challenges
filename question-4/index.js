066abddf003f9308102addf74bd41e5fbfd3849c
mugisha <mohamugi@gmail.com>
2024-05-22 14:47:04 +0300

    solution to question4

--- question-4/index.js
+++ question-4/index.js
@@ -1,18 +1,28 @@
-/*Write a function named firstPalindrome which takes a parameter: words. Given an array of string words, return the first palindromic string in the array. If there is no such string, return an empty string "".
+/*Write a function named firstPalindrome which takes a parameter:
+ words. Given an array of string words, return the first palindromic string in the array.
+  If there is no such string, return an empty string "".
 A string is palindromic if it reads the same forward and backward.
  */
 
 let words = ["abc", "car", "ada", "racecar", "cool"];
-let size = words.length;
+
+// create a function to check if the string is a palindrome
+function isPalindrome(string) {
+    return string === string.split("").reverse().join("");
+}
 // create  isPalindrome function
-// function isPalindrome(words) {
-//     // replicate string words into string items
-//     let items = words;
-//     words = words, split("").reverse().join("");
-//     // check if two strings are equal or not
-//     return words == items;
-// }
-// let result = isPalindrome(words)
-// console.log(result) 
-// creating a function firstPalindrome
+function firstPalindrome(words) {
+    // iterate through each individual string in a reversed form
+    for (let word of words) {
+        // check if word is a palindrome
+        if (isPalindrome(word)) {
+            return word;
+    }    
+    }
+    return "";
+}
+result = firstPalindrome(words);
+console.log(result)
+ 
+
  
